d65b40b77e9f86d5ee97ac42a66b4bf9a9d3b57b
RRJos <RRJos@JARR>
2024-11-03 15:56:09 -0600

    03-Agregar sdk de productos

---
+++ src/FakeStore/Products/Sdks/IFakestoreProductSdk.cs
@@ -0,0 +1,52 @@
+﻿using FakeStore.Products.Commands.Products;
+using Refit;
+
+namespace FakeStore.Products.Sdks
+{
+    public interface IFakestoreProductSdk
+    {
+        /// <summary>
+        /// Url base de los endpoints de productos
+        /// </summary>
+        const string URL = "/products";
+
+        /// <summary>
+        /// Devuelve todos los productos
+        /// </summary>
+        /// <param name="limit">Limite de productos</param>
+        /// <param name="sort">Ordernamiento</param>
+        /// <returns></returns>
+        [Get(URL)]
+        Task<List<ProductOutput>> GetProducts([Query] int? limit,
+                                              [Query] string sort);
+
+        /// <summary>
+        /// Devuelve un producto por Id
+        /// </summary>
+        /// <returns></returns>
+        [Get(URL + "/{id}")]
+        Task<ProductOutput> GetProductById(int Id);
+
+        /// <summary>
+        /// Crea un producto
+        /// </summary>
+        /// <returns></returns>
+        [Post(URL)]
+        Task<ProductOutput> CreateProduct(CreateProduct createProduct);
+
+        /// <summary>
+        /// Actualiza un producto
+        /// </summary>
+        /// <returns></returns>
+        [Post(URL + "/{id}")]
+        Task<ProductOutput> UpdateProduct(int Id, UpdateProduct createProduct);
+
+
+        /// <summary>
+        /// Devuelve todas las categorias
+        /// </summary>
+        /// <returns></returns>
+        [Get(URL + "/categories")]
+        Task<List<string>> GetCategories();
+    }
+}
